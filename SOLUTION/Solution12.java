d038121b8b556b8f01f50deafa3b69b98eb1cecd
21bca67 <96829494+21bca67@users.noreply.github.com>
2023-04-04 10:28:56 +0530

    Create Solution12.java

---
+++ SOLUTION/Solution12.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.applet.*;
+
+/*<applet code="Program10.class" height="800" width="1860"> 
+	<param name="numOvals" value="10">
+  </applet>*/
+
+public class Program10 extends Applet {
+    private int numOvals;
+
+    public void init() {
+        String numOvalsStr = getParameter("numOvals");
+        numOvals = Integer.parseInt(numOvalsStr);
+    }
+
+    public void paint(Graphics g) {
+        for (int i = 0; i < numOvals; i++) {
+            int x = (int)(Math.random() * 300);
+            int y = (int)(Math.random() * 300);
+            int w = (int)(Math.random() * 100);
+            int h = (int)(Math.random() * 100);
+            g.drawOval(x, y, w, h);
+        }
+    }
+}
